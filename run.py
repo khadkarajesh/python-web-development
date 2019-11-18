6e05dab1d76810a7ac781d94e12d228d033b8c64
Rajesh <rajesh.k.khadka@gmail.com>
2019-11-18 20:41:57 +0545

    add hello world

--- run.py
+++ run.py
@@ -1,4 +1,17 @@
+from flask import Flask
+from flask_restful import Api, Resource
 
+app = Flask(__name__)
+api = Api(app, prefix='/v1')
+
+
+class HelloWorldResource(Resource):
+    @classmethod
+    def get(cls):
+        return 'Congratulations you have successfully built first api in flask'
+
+
+api.add_resource(HelloWorldResource, '')
 
 if __name__ == '__main__':
-    print('')
+    app.run(debug=True)
